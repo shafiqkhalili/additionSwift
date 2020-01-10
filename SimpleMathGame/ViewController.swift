458e70a718502288ea9f29c997b169e28ebedfbe
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-01-10 13:50:15 +0100

    Initial Commit

---
+++ SimpleMathGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMathGame
+//
+//  Created by Shafigh Khalili on 2020-01-10.
+//  Copyright © 2020 Shafigh Khalili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
